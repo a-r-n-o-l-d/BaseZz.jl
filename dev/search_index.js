ddcf6e1cda87e8f1d74877a35de7f0c73207a308
Documenter.jl <documenter@juliadocs.github.io>
2025-06-02 16:45:11 +0000

    build based on e051e6f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#BaseZz","page":"Home","title":"BaseZz","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BaseZz.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
