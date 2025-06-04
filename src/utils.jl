1afd7f05fedb36555faaf1540d576a9ce4856956
a-r-n-o-l-d <arnold.fertin@univ-grenoble-alpes.fr>
2025-06-04 10:56:05 +0200

    documentation

--- src/utils.jl
+++ src/utils.jl
@@ -52,6 +52,47 @@ isnumber(x) = !isnotnumber(x)
 
 # as_finite(x) = skip(isnan || !isfinite, x)
 
+"""
+    fastextrema(x) -> (mini, maxi)
+
+Compute the minimum and maximum values of an input array `x` and return them
+as a tuple.
+
+`x` can be an array of `Real` numbers, `Gray` colorants, or multi-channel
+colorants `Colorant{T,N}`, where `T` is a `Real` type. If `x` is an array of
+`AbstractFloat` elements, values such as `NaN`, `missing`, `Inf`, or `-Inf` are
+automatically excluded from the computation. Additionally, `x` can be a `Skip`
+object from the [Skipper.jl](https://github.com/JuliaAPlavin/Skipper.jl)
+package, created using the `skip` or `keep` functions.
+
+# Examples
+```julia
+# Example with an array of real numbers
+data = [1.0, 2.0, 3.0, 4.0, 5.0]
+mini, maxi = fastextrema(data)
+println("Minimum: ", mini, ", Maximum: ", maxi)
+
+# Example with a multi-channel image
+img = rand(RGB, 10, 10) # Random RGB image
+mini, maxi = fastextrema(img)
+println("Minimum: ", mini, ", Maximum: ", maxi)
+
+# Example with an 8-bit grayscale image, ignoring all values below 0.5
+img = rand(Gray{N0f8}, 10, 10)
+mini, maxi = skip(x -> x < 0.5, img) |> fastextrema
+println("Minimum: ", mini, ", Maximum: ", maxi)
+```
+
+# Note
+Microbenchmarks indicate that fastextrema is about twice as fast as
+Base.extrema for floating-point numbers, but shows no significant benefit for
+integers. The advantage of fastextrema lies in its support for Skipper.jl.
+However, it is less versatile than Base.extrema as it lacks the dims
+argument and does not support generic iterable collections. Despite this,
+fastextrema remains sufficient for image processing tasks.
+"""
+fastextrema
+
 # 2 times faster than Base.extrema for float thanks to Skipper.jl (no big benefit for Int), but maybe unsafe
 # a little faster than minimum or maximum for float, two times faster for Int (not sure)
 # To do: design clean microbenchmarks (on other ubuntu machine)
